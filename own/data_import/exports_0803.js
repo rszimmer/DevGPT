8adbbd67f0f94dc36080135401eb90991e801973
zimmer.raquel <zimmer.raquel@scalasystems.com.br>
2024-11-02 17:00:37 -0300

    versão da visualização arquivos em txt mostrando cada coisa separadamente

---
+++ own/data_import/exports_0803.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const jsonsInDir = fs
+  .readdirSync("../snapshot_20230803")
+  .filter((file) => path.extname(file) === ".json");
+
+const objHelper = {};
+const filesNameList = ["PR", "ISSUE", "DISCUSSION", "COMMIT", "FILE", "HN"];
+
+jsonsInDir.forEach((file) => {
+  const fileData = fs.readFileSync(path.join("../snapshot_20230803", file));
+  const name = filesNameList.filter((e) => file.toUpperCase().includes(e))[0];
+  const json = JSON.parse(fileData.toString());
+  objHelper[name] = json;
+});
+
+module.exports = objHelper;
